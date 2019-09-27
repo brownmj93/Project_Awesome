78c0d68c473522f229f9bd0f92a1432798c6dab8
jckent <chasekent30@gmail.com>
2019-09-26 20:57:04 -0400

    added donut chart html/js/cdn

--- assets/app.js
+++ assets/app.js
@@ -156,3 +156,23 @@ $.ajax(settings).done(function (response) {
   
 });
 // ************** ABOVE LIES THE NUTRITION ****************//
+
+//chartjs//
+
+new Chart(document.getElementById("doughnut-chart"), {
+  type: 'doughnut',
+  data: {
+      labels: [],
+      datasets: [{
+          label: "Calories by ingredient",
+          backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+          data: []
+      }]
+  },
+  options: {
+      title: {
+          display: true,
+          text: 'Calories by ingredient'
+      }
+  }
+});
